d4ae8590362bdc907a9bbc84fbab9821a1e7c11f
jin songguang <jinsongguang@163.com>
2018-11-28 14:08:45 +0800

    修改启动文件

--- src/main/java/com/example/demo/MytestApplication.java
+++ src/main/java/com/example/demo/MytestApplication.java
@@ -2,10 +2,20 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.extern.slf4j.Slf4j;
 @SpringBootApplication
+@Slf4j
 public class MytestApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MytestApplication.class, args);
+		log.info("run......");
+		System.out.println("run......");
+	}
+	@RequestMapping("/")
+	public String hello() {
+	    return "Hello world";
 	}
 }
