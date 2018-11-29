a8d123f548d8c6e1f31835972dd4312f194a9020
jin songguang <jinsongguang@163.com>
2018-11-29 09:25:57 +0800

    修改启动文件

--- src/main/java/com/example/demo/MytestApplication.java
+++ src/main/java/com/example/demo/MytestApplication.java
@@ -3,10 +3,12 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import lombok.extern.slf4j.Slf4j;
 @SpringBootApplication
 @Slf4j
+@RestController
 public class MytestApplication {
 
 	public static void main(String[] args) {
@@ -15,6 +17,7 @@ public class MytestApplication {
 		System.out.println("run......");
 	}
 	@RequestMapping("/")
+	//@ResponseStatus(value = HttpStatus.FORBIDDEN)
 	public String hello() {
 	    return "Hello world";
 	}
